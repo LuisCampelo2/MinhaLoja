d2dfaaa0f2dbd6da967cb7c0b7b2ebd62f121ec2
Luis Campelo <luisfilipecab@gmail.com>
2025-01-06 02:19:45 -0300

    criado_create_account

--- loja/urls.py
+++ loja/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name='loja'
+
 urlpatterns = [
-    path('',views.index)
+    path('',views.index,name='index'),
+    path('create_account',views.createAccount,name='create_account'),
 ]
--- loja/views.py
+++ loja/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+from django.urls import reverse
+from .forms import RegisterUser
+from django.contrib import auth
+from django.shortcuts import redirect
 
 # Create your views here.
 def index(request):
     return render(request, 'loja/pages/index.html')
+
+def createAccount(request):
+    form_action=reverse('loja:create_account')
+    form=RegisterUser()
+    
+    if request.method=='POST':
+        form=RegisterUser(request.POST)
+        user = form.save(commit=False)  # Cria o usuário sem salvar diretamente.
+        user.set_password(form.cleaned_data['password1'])  # Define a senha corretamente.
+        user.save()  # Salva o usuário no banco.
+        auth.login(request, user)  # Faz o login automático.
+        return redirect('home:index')  
+            
+        
+    context = {
+        'form': form,
+        'form_action': form_action
+    }
+    return render(request,'loja/pages/create_account.html',context)
