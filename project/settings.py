5fad3ea527f6e85d319ab553106c8572269d9e37
Luis Campelo <luisfilipecab@gmail.com>
2025-02-11 17:34:41 -0300

    .

--- project/settings.py
+++ project/settings.py
@@ -76,19 +76,15 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-import os
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': os.getenv('DB_ENGINE'),
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
-        'OPTIONS': {
-            'client_encoding': 'UTF8',
-        },
     }
 }
 
